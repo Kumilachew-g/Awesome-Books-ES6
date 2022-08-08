3d89d657ee90a08b5c505be84a7c8899e7fd5802
kumilachew-g <kumilachew1212@gmail.com>
2022-08-08 22:15:11 +0300

    add js files

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-/* eslint-disable max-classes-per-file */
+import {
+  addBtn,
+  addLink,
+  authorInput,
+  booksContainer,
+  booksList,
+  contactLink,
+  contactSection,
+  listLink,
+  newBook,
+  titleInput,
+  year,
+} from './modules/elements.js';
+import BooksClass from './modules/book.js';
 
-// ----------------- Variables
-
-// Variable to hold the outputs
-const booksContainer = document.getElementById('books-container');
-// Variables to hold the input ids
-const titleInput = document.getElementById('title-input');
-const authorInput = document.getElementById('author-input');
-
-// new
-const navListLink = document.getElementById('book-list');
-const addBookLink = document.getElementById('add-book');
-const contactNavlink = document.getElementById('contacts');
-const year = document.getElementById('date');
-
-// Create an array of objects for the books information
-class BooksClass {
-  constructor() {
-    return [];
-  }
-}
-// Create a new books object
+// import Book from './modules/bookClass.js';
 const books = new BooksClass();
-
-// Books array to store the books information
-class Book {
+export default class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
@@ -33,10 +24,15 @@ class Book {
   // Method to load newly added books
   static loadBook(index) {
     booksContainer.innerHTML += `<div class="book-card">
-      <div class="book-title"><strong>"${books[index].title}"</strong>&nbsp;by</div>
-      <div class="book-author">${books[index].author}</div>
-      <button class="card-remove-button" onclick="Book.removeCard(${index})">Remove</button>
-      </div>`;
+        <div class="book-title"><strong>"${books[index].title}"</strong>&nbsp;by</div>
+        <div class="book-author">${books[index].author}</div>
+        <button class="card-remove-button" >Remove</button>
+        </div>`;
+
+    const removeBtn = document.querySelectorAll('.card-remove-button');
+    removeBtn.forEach((button, index) => button.addEventListener('click', () => {
+      Book.removeCard(index);
+    }));
   }
 
   // A Method to remove current object from the array
@@ -51,27 +47,25 @@ class Book {
     booksContainer.innerHTML = '';
     for (let index = 0; index < books.length; index += 1) {
       booksContainer.innerHTML += `<div class="book-card">
-      <div class="book-title"><strong>"${books[index].title}"</strong>&nbsp;by</div>
-      <div class="book-author">${books[index].author}</div>
-      <button class="card-remove-button" onclick="Book.removeCard(${index})">Remove</button>
-      </div>`;
+        <div class="book-title"><strong>"${books[index].title}"</strong>&nbsp;by</div>
+        <div class="book-author">${books[index].author}</div>
+        <button class="card-remove-button">Remove</button>
+        </div>`;
+
+      const removeBtn = document.querySelectorAll('.card-remove-button');
+      removeBtn.forEach((button, index) => button.addEventListener('click', () => {
+        Book.removeCard(index);
+      }));
     }
   }
 }
 
-// ------------------- Functions -------------------
-
-// ------------------- get data from local storage and reload the books cards
-// Parse object from localStorage and store to a variable
 const storedBooks = JSON.parse(localStorage.getItem('books'));
-// If there is a storedBooks array, set the books array to the storedBooks array
 if (storedBooks) {
   books.push(...storedBooks);
   Book.reloadBooks();
 }
 
-// A click listener for the add button to add inputs value as an object to the books array
-const addBtn = document.querySelector('#add-btn');
 addBtn.addEventListener('click', () => {
   const newBook = new Book(titleInput.value, authorInput.value);
   books.push(newBook);
@@ -82,16 +76,6 @@ addBtn.addEventListener('click', () => {
 const dateTime = new Date(Date.now());
 year.textContent = dateTime.toUTCString();
 
-// ------------------- Navigation -------------------
-// Variables to hold the navigation links
-const listLink = document.querySelector('#list-link');
-const addLink = document.querySelector('#add-link');
-const contactLink = document.querySelector('#contact-link');
-const booksList = document.querySelector('#books-list');
-const newBook = document.querySelector('#new-book');
-const contactSection = document.querySelector('#contact-section');
-
-// A click listener for the list link to show the books cards
 listLink.addEventListener('click', () => {
   booksList.style.display = 'block';
   listLink.style.color = 'blue';
@@ -101,7 +85,6 @@ listLink.addEventListener('click', () => {
   contactLink.style.color = 'black';
 });
 
-// A click listener for the add link to show the add book form
 addLink.addEventListener('click', () => {
   booksList.style.display = 'none';
   addLink.style.color = 'blue';
@@ -111,7 +94,6 @@ addLink.addEventListener('click', () => {
   contactLink.style.color = 'black';
 });
 
-// A click listener for the contact link to show the contact form
 contactLink.addEventListener('click', () => {
   booksList.style.display = 'none';
   contactLink.style.color = 'blue';
